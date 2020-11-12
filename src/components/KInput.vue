0ddbeb346ff948624fcc8ae71fce90b9fc5f9494
wangmeijuan <wangmeijuan@ablesky.com>
2020-11-12 20:14:44 +0800

    feat:实现v-model

---
+++ src/components/KInput.vue
@@ -0,0 +1,21 @@
+<template>
+  <input @input="onInput" :value="value" v-bind="$attrs"/>
+</template>
+
+<script>
+export default {
+    inheritAttrs: false,
+    data() {
+        return {
+            value: ''
+        }
+    },
+    methods: {
+        onInput(e) {
+            this.$emit('input', e.target.value);
+        }
+    }
+};
+</script>
+
+<style></style>
